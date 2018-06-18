79aa9c50928adf7dba83e75680d953dc11768be6
Dongzy <dzy201415@gmail.com>
2018-06-18 13:18:34 +0800

    reconstruct of main loop

--- CAS_Auto_Login.py
+++ CAS_Auto_Login.py
@@ -19,12 +19,6 @@ from requests.exceptions import RetryError
 
 # os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
 
-def load_config():
-    with open('./config.json') as f:
-        config = json.load(f)
-    return config
-config = load_config()
-
 try:
     with open("logging.yaml", 'rt') as f:
         config = yaml.safe_load(f.read())
@@ -100,74 +94,76 @@ def test_network(url):
             raise BaseHTTPError("Invalid status code {code}".format(code=test.status_code))
 
 
-def main():
-
-    global config
-    config = load_config()
-    logger.debug('Configurations successfully imported.')
-    
-    times_retry_login = config['max_times_retry_login']
-    test_url = config['captive_portal_server']
-    
+def wait_network(url, waiting_interval):
     while True:
-        logger.debug('Checking network status...')
-        try:
-            link = test_network(test_url)
-            if not link:
-                logger.debug('You are already logged in.')
-                return
-            else:
-                content = requests.get(link, timeout=10).content
-                soup_login = BeautifulSoup(content, 'html5lib')
-                
-                if 'CAS' not in soup_login.title.string:
-                    logger.warning('Not connected to a SUSTC network')
-                    return
-                
-                logger.info('You are offline. Starting login...')
-                
-                rem_link = re.search(r'window\.location = \'(.*)\';', soup_login.text).group(1)
-                hostname = 'http://enet.10000.gd.cn:10001/sz/sz112/'
-                service = hostname + rem_link
-                
-                success, err = do_login(service, config['username'], config['password'])
-                
-                if err:
-                    logger.error('Error occurred: %s', str(err))
-                elif success:
-                    logger.info('Login successful')
-                    
-                    # define the orperation after login
-                    try:
-                        hot_load("post_login").run()
-                    except ModuleNotFoundError:
-                        pass
-                    except Exception as e:
-                        logger.error("Error in executing run() in post_login: %s",e , exc_info=True)
-                    
-                    return
-        except RequestException as err:
-            logger.warn('Network FAILED.')
-        
-        # If keep trying to login too many times, it may trigger security alarm on the CAS server
-        times_retry_login -= 1
-        logger.info('{attempt} attempt(s) remaining.'.format(attempt=times_retry_login))
-        if times_retry_login <= 0:
-            raise RetryError
-        logger.info('Try again in {time} sec. '.format(time=config['interval_retry_connection']))
-        sleep(config['interval_retry_connection'])
+        link = test_network(url)
+        if not link is None:
+            return link
+        sleep(waiting_interval)
 
+
+def load_config():
+    with open('./config.json') as f:
+        config = json.load(f)
+    return config
+
+
+def main():
+    logger.info('Program started.')
+
+    while True:
+        config = load_config()
+        logger.debug('Configurations successfully imported.')
+
+        try:
+            logger.info('Checking network status... [every %d sec]', config['interval_check_network'])
+            link = wait_network(config['captive_portal_server'], config['interval_check_network'])
+
+            red_page = requests.get(link, timeout=10)
+            content = red_page.content
+            red_page.close()
+
+            soup_login = BeautifulSoup(content, 'html5lib')
+            
+            if 'CAS' not in soup_login.title.string:
+                logger.warning('Not connected to a SUSTC network')
+                sleep(config['interval_check_network'])
+                continue
+            
+            logger.info('You are offline. Starting login...')
+            
+            rem_link = re.search(r'window\.location = \'(.*)\';', soup_login.text).group(1)
+            hostname = 'http://enet.10000.gd.cn:10001/sz/sz112/'
+            service = hostname + rem_link
+            
+            success, err = do_login(service, config['username'], config['password'])
+            
+            if err:
+                logger.error('Error occurred: %s', str(err))
+                logger.info('Try again in {time} sec. '.format(time=config['interval_retry_connection']))
+                sleep(config['interval_retry_connection'])
+            elif success:
+                logger.info('Login successful')
+                
+                # define the orperation after login
+                try:
+                    hot_load("post_login").run()
+                except ModuleNotFoundError:
+                    pass
+                except Exception as e:
+                    logger.error("Error in executing run() in post_login: %s",e , exc_info=True)
+                
+        except RequestException as err:
+            logger.warn('Network FAILED in %s', err.request.url)
+            sleep(config['interval_check_network'])
+        except BaseHTTPError as err:
+            logger.error('{msg}, consider updating \'captive_portal_server\''.format(msg=str(err)))
+            sleep(config['interval_check_network'])
+        
+        
 
 if __name__ == '__main__':
-    logger.info('Program started. Monitoring network...')
     try:
-        while True:
-            try:
-                main()
-            except RetryError:
-                logger.error('Attempts used up. Wait for next %d second', config['interval_check_network'])
-            sleep(config['interval_check_network'])
-    except BaseHTTPError as err:
-        logger.error('{msg}, consider updating \'captive_portal_server\''.format(msg=str(err)))
+        main()
     except Exception as e:
         logger.critical("Critical error occurs", exc_info=True)
