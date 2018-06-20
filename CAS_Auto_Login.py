7873b6af8df99ad029276e32e3e0ebebc2bac887
Dongzy <dzy201415@gmail.com>
2018-06-20 13:31:37 +0800

    run post_login script in working dir

--- CAS_Auto_Login.py
+++ CAS_Auto_Login.py
@@ -18,6 +18,7 @@ from requests.exceptions import RequestException
 from requests.exceptions import RetryError
 
 # os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
+sys.path.insert(0, '')
 
 try:
     with open("logging.yaml", 'rt') as f:
